0c2fa2c86d5ff736dba2239265ffc9f406876beb
TonyCode2012 <TonyCode2012@126.com>
2023-01-09 17:36:44 +0800

    Add cross domain

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,14 @@ const loggingResponse = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json());
